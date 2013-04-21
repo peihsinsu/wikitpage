945ff4b968fd6246bf2dfa59891e3a213df4151c
peihsinsu <simonsu.mail@gmail.com>
2013-04-21 20:15:18 +0800

    update broken link of no page specify

--- wikitpage.js
+++ wikitpage.js
@@ -3,6 +3,6 @@ var config={
   project:'wikitpage', //The github project name, that will use for build edit link
   md_file_path: '../mdfiles', //The md file folder related path that for read md file use
   md_folder_name: 'mdfiles', //The md folder name, use for open the edit url from github
-  welcome_page: 'README.md', //The welcome page, default location that access from root url
+  welcome_page: '../README.md', //The welcome page, default location that access from root url
   menu_page: 'Menu.md' //The menu page, that shows in the left of the page
 };
